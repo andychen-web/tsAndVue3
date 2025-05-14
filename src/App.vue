7fe3d0eb6d1703d7d1dc76e60379c38c3f5dbc47
andychen <descartes1960s@gmail.com>
2025-05-14 19:56:40 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import BlockList from "./components/junior/BlockList.vue"
+</script>
+
+<template>
+  <BlockList msg="Vite + Vue" /> 
+</template>
+
+<style scoped>
+</style>
